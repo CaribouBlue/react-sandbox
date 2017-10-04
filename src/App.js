a687f93ea0e54b43e220142728e601eb0e9a5c90
Alec Draymore <Draymore26@gmail.com>
2017-10-04 19:15:40 -0400

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import speech from './speech-syn/speech';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      voiceURI: 'Google US English',
+    };
+
+    this.bindings(
+      'optionChange',
+      'inputChange',
+      'submit',
+    );
+  }
+
+  bindings(...args) {
+    args.forEach(func => {
+      this[func] = this[func].bind(this);
+    });
+  }
+
+  optionChange(e) {
+    e.preventDefault();
+    this.setState({ voiceURI: e.target.value });
+  }
+
+  inputChange(e) {
+    e.preventDefault();
+    this.setState({ input: e.target.value });
+  }
+
+  submit(e) {
+    e.preventDefault();
+    speech.sayIt(this.state.input, this.state.voiceURI)
+  }
+
   render() {
+    const appStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      width: '100vw',
+      alignItems: 'center',
+      marginTop: '50px'
+    };
+
     return (
-      <div>
+      <div
+
+      >
+        <form
+          onSubmit={this.submit}
+          style={appStyle}
+        >
+          <select
+            value={this.state.voiceURI}
+            onChange={this.optionChange}
+          >
+            {
+              speech.getVoiceOptions().map(voice => (
+                <option
+                  value={voice}
+                  key={_.uniqueId()}
+                >
+                  {voice}
+                </option>
+              ))
+            }
+          </select>
+          <textarea
+            rows="5"
+            cols="50"
+            onChange={this.inputChange}
+          >
+          </textarea>
+          <button
+            type="submit"
+          >Say it!</button>
+        </form>
       </div>
     );
   }
